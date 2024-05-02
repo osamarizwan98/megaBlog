104c03b1ae1d56d754f33a47126ec9fe164583c1
osamarizwan98 <osamarizwan98@gmail.com>
2024-05-02 23:27:02 +0500

    upload megablog project

---
+++ src/components/RTE.jsx
@@ -0,0 +1,59 @@
+import { Editor } from "@tinymce/tinymce-react";
+import React from "react";
+import { Controller } from "react-hook-form";
+
+function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            apiKey='1factnz5nipkej09z7t7zez6xwv9ve3yyaeq4qb2ngdj6ihr'
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
+
+export default RTE;
